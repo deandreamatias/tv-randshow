c08bf822c0a33c6c8122a5b1dec8e217f6fa5839
Matias de Andrea <deandreamatias@gmail.com>
2019-08-15 20:58:48 +0200

    Create a api provider

--- lib/src/resources/api_provider.dart
+++ lib/src/resources/api_provider.dart
@@ -1,3 +1,12 @@
+import 'package:http/http.dart' as http;
+
 class ApiProvider {
-  
+  Future<http.Response> requestPost(String url) async {
+    http.Response response = await http.post(url);
+    return response;
+  }
+  Future<http.Response> requestGet(String url) async {
+    http.Response response = await http.get(url);
+    return response;
+  }
 }
