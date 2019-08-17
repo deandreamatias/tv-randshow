934bdb83282ebee18c77a13dccf26dcd24de5fdd
Matias de Andrea <deandreamatias@gmail.com>
2019-08-17 23:00:08 +0200

    Populate base model

--- lib/src/models/base_model.dart
+++ lib/src/models/base_model.dart
@@ -1,5 +1,26 @@
 import 'package:scoped_model/scoped_model.dart';
 
+enum BaseState { init, loading, error }
+
 class BaseModel extends Model {
-  
+  BaseState _state;
+  BaseState get state => _state;
+
+  setInit() {
+    _setState(BaseState.init);
+  }
+
+  setLoading() {
+    _setState(BaseState.loading);
+  }
+
+  setError() {
+    _setState(BaseState.error);
+  }
+
+  _setState(BaseState baseState) {
+    _state = baseState;
+    print('# State of $runtimeType: $baseState');
+    notifyListeners();
+  }
 }
