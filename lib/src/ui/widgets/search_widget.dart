a1f46af120c1dc7d3b9c2abe378f7228a33400f0
Matias de Andrea <deandreamatias@gmail.com>
2019-08-17 22:55:13 +0200

    Create search view

---
+++ lib/src/ui/widgets/search_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tv_randshow/src/utils/styles.dart';
+
+class SearchWidget extends StatelessWidget {
+  final TextEditingController editingController;
+  const SearchWidget({Key key, this.editingController}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: TextField(
+        controller: editingController,
+        decoration: InputDecoration(
+          hintText: "Search",
+          contentPadding: EdgeInsets.all(0.0),
+          //suffixIcon: Icon(Icons.close),
+          prefixIcon: Icon(Icons.search, color: StyleColor.SECONDARY),
+          border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: StyleColor.SECONDARY)),
+        ),
+      ),
+    );
+  }
+}
