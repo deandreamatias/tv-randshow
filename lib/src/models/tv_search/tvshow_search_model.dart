3a512179852d1d31090da983b18a5b6bbdf7eadc
Matias de Andrea <deandreamatias@gmail.com>
2019-09-28 13:02:50 +0200

    Implement load search from API

--- lib/src/models/tv_search/tvshow_search_model.dart
+++ lib/src/models/tv_search/tvshow_search_model.dart
@@ -1,5 +1,18 @@
 import 'package:tv_randshow/src/models/base_model.dart';
+import 'package:tv_randshow/src/models/tv_search/tvshow_search.dart';
+import 'package:tv_randshow/src/ui/widgets/tvshow_widget.dart';
+import 'package:tv_randshow/src/utils/constants.dart';
 
 class TvshowSearchModel extends BaseModel {
-  
+  Future<List<TvshowWidget>> getSearch(String query) async {
+    final apiKey = await secureStorage.readStorage(KeyStorate.API_KEY);
+    var queryParameters = {'api_key': apiKey, 'language': 'en-US', 'page': '1', 'query': query};
+
+    var data = await fetchData(Url.TVSHOW_SEARCH, queryParameters);
+    List<TvshowWidget> _list = TvshowSearch.fromRawJson(data).results.map((result) {
+      return TvshowWidget(tvshowName: result.name, urlImage: result.posterPath);
+    }).toList();
+
+    return _list;
+  }
 }
