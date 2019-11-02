f2148da81645ce82dca43d951186ae14548e539b
Matias de Andrea <deandreamatias@gmail.com>
2019-11-02 12:33:30 +0100

    Clean unused parameters

--- lib/src/models/tv_search/tvshow_search.dart
+++ lib/src/models/tv_search/tvshow_search.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 class TvshowSearch {
   int page;
-  List<Result> results;
+  List<TvShowSearchItem> results;
   int totalResults;
   int totalPages;
 
@@ -19,7 +19,8 @@ class TvshowSearch {
 
   factory TvshowSearch.fromJson(Map<String, dynamic> json) => TvshowSearch(
         page: json["page"],
-        results: List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+        results:
+            List<TvShowSearchItem>.from(json["results"].map((x) => TvShowSearchItem.fromJson(x))),
         totalResults: json["total_results"],
         totalPages: json["total_pages"],
       );
@@ -32,71 +33,30 @@ class TvshowSearch {
       };
 }
 
-class Result {
+class TvShowSearchItem {
   String posterPath;
-  double popularity;
   int id;
-  String backdropPath;
-  double voteAverage;
-  String overview;
-  DateTime firstAirDate;
-  List<String> originCountry;
-  List<int> genreIds;
-  String originalLanguage;
-  int voteCount;
   String name;
-  String originalName;
 
-  Result({
+  TvShowSearchItem({
     this.posterPath,
-    this.popularity,
     this.id,
-    this.backdropPath,
-    this.voteAverage,
-    this.overview,
-    this.firstAirDate,
-    this.originCountry,
-    this.genreIds,
-    this.originalLanguage,
-    this.voteCount,
     this.name,
-    this.originalName,
   });
 
-  factory Result.fromRawJson(String str) => Result.fromJson(json.decode(str));
+  factory TvShowSearchItem.fromRawJson(String str) => TvShowSearchItem.fromJson(json.decode(str));
 
   String toRawJson() => json.encode(toJson());
 
-  factory Result.fromJson(Map<String, dynamic> json) => Result(
+  factory TvShowSearchItem.fromJson(Map<String, dynamic> json) => TvShowSearchItem(
         posterPath: json["poster_path"],
-        popularity: json["popularity"].toDouble(),
         id: json["id"],
-        backdropPath: json["backdrop_path"],
-        voteAverage: json["vote_average"].toDouble(),
-        overview: json["overview"],
-        firstAirDate: DateTime.parse(json["first_air_date"]),
-        originCountry: List<String>.from(json["origin_country"].map((x) => x)),
-        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
-        originalLanguage: json["original_language"],
-        voteCount: json["vote_count"],
         name: json["name"],
-        originalName: json["original_name"],
       );
 
   Map<String, dynamic> toJson() => {
         "poster_path": posterPath,
-        "popularity": popularity,
         "id": id,
-        "backdrop_path": backdropPath,
-        "vote_average": voteAverage,
-        "overview": overview,
-        "first_air_date":
-            "${firstAirDate.year.toString().padLeft(4, '0')}-${firstAirDate.month.toString().padLeft(2, '0')}-${firstAirDate.day.toString().padLeft(2, '0')}",
-        "origin_country": List<dynamic>.from(originCountry.map((x) => x)),
-        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
-        "original_language": originalLanguage,
-        "vote_count": voteCount,
         "name": name,
-        "original_name": originalName,
       };
 }
