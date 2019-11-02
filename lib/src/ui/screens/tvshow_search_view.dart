b9a881b687625eca6aaae62918c179fda3e76bcf
Matias de Andrea <deandreamatias@gmail.com>
2019-11-02 12:47:04 +0100

    Divide menu details widget

--- lib/src/ui/screens/tvshow_search_view.dart
+++ lib/src/ui/screens/tvshow_search_view.dart
@@ -4,10 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:tv_randshow/src/models/base_model.dart';
 import 'package:tv_randshow/src/models/tv_search/tvshow_search_model.dart';
 import 'package:tv_randshow/src/ui/widgets/backdrop_widget.dart';
-import 'package:tv_randshow/src/ui/widgets/info_box_widget.dart';
+import 'package:tv_randshow/src/ui/widgets/menu_details_widget.dart';
 import 'package:tv_randshow/src/ui/widgets/search_widget.dart';
-import 'package:tv_randshow/src/utils/constants.dart';
-import 'package:tv_randshow/src/utils/styles.dart';
 
 class TvshowSearchView extends StatefulWidget {
   TvshowSearchView({Key key}) : super(key: key);
@@ -34,7 +32,7 @@ class _TvshowSearchViewState extends State<TvshowSearchView> {
             builder: (context) => _tvshowSearchModel,
             child: Consumer<TvshowSearchModel>(builder: (context, value, child) {
               return Backdrop(
-                frontLayer: FrontPanel(),
+                frontLayer: MenuPanelWidget(),
                 backLayer: _buildListSearch(),
                 panelVisible: value.tvShowDetails,
               );
@@ -73,87 +71,5 @@ class _TvshowSearchViewState extends State<TvshowSearchView> {
         ],
       ),
     );
-  }
-}
-
-class FrontPanel extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(alignment: Alignment.topCenter, overflow: Overflow.visible, children: <Widget>[
-      Positioned(
-        top: -22.5,
-        child: RaisedButton.icon(
-          icon: Icon(Icons.star_border, color: StyleColor.PRIMARY),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-              side: BorderSide(color: StyleColor.PRIMARY)),
-          color: StyleColor.WHITE,
-          label: Text('Add to fav', style: TextStyle(color: StyleColor.PRIMARY)),
-          onPressed: () {},
-        ),
-      ),
-      Container(
-        margin: EdgeInsets.only(top: 20),
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    child: Image.asset(ImagePath.emptyTvShow),
-                    decoration: BoxDecoration(
-                      borderRadius: BORDER_RADIUS,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'Friends',
-                        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Row(
-                children: <Widget>[
-                  InfoBoxWidget(typeInfo: 0),
-                  InfoBoxWidget(typeInfo: 1),
-                  InfoBoxWidget(typeInfo: 2)
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Sinopse', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
-                  SizedBox(height: 4.0),
-                  Text('Descripción .....')
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    ]);
-  }
-
-  ImageProvider checkImage() {
-    //if (urlImage == null) {
-    return AssetImage(ImagePath.emptyTvShow);
-    //} else {
-    //return NetworkImage(Url.BASE_IMAGE + urlImage);
-    //}
   }
 }
---
+++ lib/src/ui/widgets/menu_details_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:tv_randshow/src/ui/widgets/info_box_widget.dart';
+import 'package:tv_randshow/src/utils/constants.dart';
+import 'package:tv_randshow/src/utils/styles.dart';
+
+class MenuPanelWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(alignment: Alignment.topCenter, overflow: Overflow.visible, children: <Widget>[
+      Positioned(
+        top: -22.5,
+        child: RaisedButton.icon(
+          icon: Icon(Icons.star_border, color: StyleColor.PRIMARY),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              side: BorderSide(color: StyleColor.PRIMARY)),
+          color: StyleColor.WHITE,
+          label: Text('Add to fav', style: TextStyle(color: StyleColor.PRIMARY)),
+          onPressed: () {},
+        ),
+      ),
+      Container(
+        margin: EdgeInsets.only(top: 20),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Image.asset(ImagePath.emptyTvShow),
+                    decoration: BoxDecoration(
+                      borderRadius: BORDER_RADIUS,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Friends',
+                        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                children: <Widget>[
+                  InfoBoxWidget(typeInfo: 0),
+                  InfoBoxWidget(typeInfo: 1),
+                  InfoBoxWidget(typeInfo: 2)
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Sinopse', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+                  SizedBox(height: 4.0),
+                  Text('Descripción .....')
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]);
+  }
+}
