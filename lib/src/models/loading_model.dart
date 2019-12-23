f3cd27a7c03827c4721ad917ab5e02d6d6502ed0
Matias de Andrea <deandreamatias@gmail.com>
2019-12-23 20:36:30 +0100

    Clean code

--- lib/src/models/loading_model.dart
+++ lib/src/models/loading_model.dart
@@ -10,26 +10,36 @@ import 'package:tv_randshow/src/utils/constants.dart';
 class LoadingModel extends BaseModel {
   Future<TvshowResult> getEpisode(TvshowDetails tvshowDetails) async {
     setLoading();
-    final String apiKey = await secureStorage.readStorage(KeyStorate.API_KEY);
-    final Map<String, String> queryParameters = <String, String>{'api_key': apiKey};
-    final int randomSeason = _getRandomNumber(tvshowDetails.numberOfSeasons, true);
-    final dynamic data = await fetchData(
+    final String apiKey = await secureStorage.readStorage(KeyStore.API_KEY);
+    final Map<String, String> queryParameters = <String, String>{
+      'api_key': apiKey
+    };
+    final int randomSeason =
+        _getRandomNumber(tvshowDetails.numberOfSeasons, true);
+    final String data = await fetchData(
         TVSHOW_DETAILS +
             tvshowDetails.id.toString() +
             TVSHOW_DETAILS_SEASON +
             randomSeason.toString(),
         queryParameters);
-    final TvshowSeasonsDetails tvshowSeasonDetails = TvshowSeasonsDetails.fromRawJson(data);
-    final Episode episode = tvshowSeasonDetails.episodes.elementAt(
-        _getRandomNumber(tvshowSeasonDetails.episodes.length, false));
-    setInit();
-    final TvshowResult tvshowResult = TvshowResult(
-        tvshowDetails: tvshowDetails,
-        randomSeason: episode.seasonNumber,
-        randomEpisode: episode.episodeNumber,
-        episodeName: episode.name,
-        episodeDescription: episode.overview);
-    return tvshowResult;
+    if (data != null && data.isNotEmpty) {
+      final TvshowSeasonsDetails tvshowSeasonDetails =
+          TvshowSeasonsDetails.fromRawJson(data);
+      final Episode episode = tvshowSeasonDetails.episodes.elementAt(
+          _getRandomNumber(tvshowSeasonDetails.episodes.length, false));
+
+      final TvshowResult tvshowResult = TvshowResult(
+          tvshowDetails: tvshowDetails,
+          randomSeason: episode.seasonNumber,
+          randomEpisode: episode.episodeNumber,
+          episodeName: episode.name,
+          episodeDescription: episode.overview);
+      setInit();
+      return tvshowResult;
+    } else {
+      setError();
+      return null;
+    }
   }
 
   /// If total is a number from 1, add + 1 to result.
@@ -37,7 +47,7 @@ class LoadingModel extends BaseModel {
   int _getRandomNumber(int total, bool isSeason) {
     final Random random = Random();
     final int randomNumber = random.nextInt(total);
-    print('Random number nº: $randomNumber');
+    logger.printDebug('Random number nº: $randomNumber');
     return isSeason ? randomNumber + 1 : randomNumber;
   }
 }
