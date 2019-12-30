633127beab5298ca2954890b7824651be657eac9
Matias de Andrea <deandreamatias@gmail.com>
2019-12-30 17:28:27 +0100

    Clean unused consts

--- lib/src/utils/styles.dart
+++ lib/src/utils/styles.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 
 class StyleColor {
   static const Color PRIMARY = Color(0xFFE40505);
-  static const Color SECONDARY = Color(0xFF002984);
-  static const Color TEXT_HIGH = Color(0xDE000000);
-  static const Color TEXT_MEDIUM = Color(0x99000000);
-  static const Color TEXT_DISABLE = Color(0x61000000);
   static const Color WHITE = Color(0xFFFFFFFF);
 }
 
