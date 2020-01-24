7c9a7628a439c58b06945f12cd0f168ec3ce10ad
Matias de Andrea <deandreamatias@gmail.com>
2020-01-24 18:40:14 +0100

    Refactor api service

---
+++ lib/core/models/query.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
+
+part 'query.g.dart';
+
+@JsonSerializable(nullable: true, includeIfNull: false)
+class Query {
+  Query({
+    this.apiKey,
+    this.language,
+    this.query,
+    this.page,
+  });
+
+  factory Query.fromRawJson(String str) => _$QueryFromJson(json.decode(str));
+  factory Query.fromJson(Map<String, dynamic> json) => _$QueryFromJson(json);
+
+  @JsonKey(name: 'api_key')
+  String apiKey;
+  String language;
+  String query;
+  int page;
+
+  Map<String, dynamic> toJson() => _$QueryToJson(this);
+  String toRawJson() => json.encode(_$QueryToJson(this));
+}
--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,13 +1,55 @@
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
-class ApiProvider {
-  Future<http.Response> requestPost(String url) async {
-    final http.Response response = await http.post(url);
-    return response;
+import '../models/query.dart';
+import '../models/search.dart';
+import '../utils/constants.dart';
+import 'log_service.dart';
+
+class ApiService {
+  final LogService _logger = LogService.instance;
+
+  Future<Search> getSearch(Query query) async {
+    final Uri uri = Uri.https(apiUrl, TVSHOW_SEARCH, query.toJson());
+    final Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      return Search.fromRawJson(response?.body);
+    } else {
+      _logger.logger.e(
+          'Error to fetch data: ${response.reasonPhrase}', response.statusCode);
+      return null;
+    }
   }
 
-  Future<http.Response> requestGet(String url) async {
-    final http.Response response = await http.get(url);
-    return response;
+  Future<Search> getDetailsTv(Query query, int idTv) async {
+    final Uri uri = Uri.https(apiUrl, '$TVSHOW_DETAILS$idTv', query.toJson());
+    final Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      return Search.fromRawJson(response?.body);
+    } else {
+      _logger.logger.e(
+          'Error to fetch data: ${response.reasonPhrase}', response.statusCode);
+      return null;
+    }
+  }
+
+  Future<Search> getDetailsTvSeasons(
+      Query query, int idTv, int idSeason) async {
+    final Uri uri = Uri.https(
+      apiUrl,
+      '$TVSHOW_DETAILS$idTv$TVSHOW_DETAILS_SEASON$idSeason',
+      query.toJson(),
+    );
+    final Response response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      return Search.fromRawJson(response?.body);
+    } else {
+      _logger.logger.e(
+          'Error to fetch data: ${response.reasonPhrase}', response.statusCode);
+      return null;
+    }
   }
 }
