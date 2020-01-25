5452b48220b9192c4eaad3d9b8bb3e26bd2ce4dd
Matias de Andrea <deandreamatias@gmail.com>
2020-01-25 12:17:54 +0100

    Refactor widgets used in search view

---
+++ lib/ui/widgets/cached_image.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+import '../../core/utils/constants.dart';
+import '../shared/styles.dart';
+
+class CachedImage extends StatelessWidget {
+  const CachedImage({this.name, this.url, this.isModal});
+  final String name;
+  final String url;
+  final bool isModal;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: url != null
+          ? url.isNotEmpty ? BASE_IMAGE + url : Images.PLACE_HOLDER
+          : Images.PLACE_HOLDER,
+      imageBuilder:
+          (BuildContext context, ImageProvider<dynamic> imageProvider) =>
+              Container(
+        height: isModal ? null : 128.0,
+        width: isModal ? null : 144.0,
+        child: isModal
+            ? null
+            : Align(
+                alignment: Alignment.topLeft,
+                child: Container(
+                  padding: SMALL_INSESTS,
+                  decoration: BoxDecoration(
+                    borderRadius: BORDER_RADIUS,
+                    color: Colors.black54,
+                  ),
+                  child: Text(
+                    name,
+                    style: StyleText.NAME,
+                  ),
+                ),
+              ),
+        decoration: BoxDecoration(
+          borderRadius: BORDER_RADIUS,
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: imageProvider,
+          ),
+        ),
+      ),
+      placeholder: (BuildContext context, String url) => Container(
+        // TODO: Invert horizontal and vertical padding
+        padding: const EdgeInsets.symmetric(horizontal: 48.0, vertical: 36.0),
+        child: const CircularProgressIndicator(),
+      ),
+      errorWidget: (BuildContext context, String url, Object error) =>
+          Image.asset(Images.EMPTY_IMAGE),
+    );
+  }
+}
---
+++ lib/ui/widgets/modal_sheet.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:provider/provider.dart';
+
+import '../../core/viewmodels/widgets/details_model.dart';
+import '../base_widget.dart';
+import '../shared/styles.dart';
+import 'cached_image.dart';
+import 'info_box.dart';
+import 'save_button.dart';
+
+class ModalSheet extends StatelessWidget {
+  const ModalSheet({this.idTv, this.inDatabase});
+  final int idTv;
+  final bool inDatabase;
+  // TODO(deandreamatias): Persist state of button on search and fav widgets
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseWidget<DetailsModel>(
+      model: DetailsModel(
+        apiService: Provider.of(context),
+        secureStorageService: Provider.of(context),
+      ),
+      onModelReady: (DetailsModel model) => model.getDetails(
+        idTv,
+        FlutterI18n.currentLocale(context).languageCode.toString(),
+      ),
+      builder: (BuildContext context, DetailsModel model, Widget child) =>
+          Stack(
+        alignment: Alignment.topCenter,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(top: 24),
+            padding: DEFAULT_INSESTS,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadiusDirectional.vertical(
+                top: Radius.circular(16.0),
+              ),
+              color: StyleColor.WHITE,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 6.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: CachedImage(
+                              url: model.tvshowDetails.posterPath,
+                              isModal: true),
+                        ),
+                        const SizedBox(width: 8.0),
+                        Expanded(
+                          flex: 3,
+                          child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              model.tvshowDetails.name,
+                              softWrap: true,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                              style: StyleText.TITLE,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Row(
+                    children: <Widget>[
+                      InfoBox(
+                        typeInfo: 0,
+                        value: model.tvshowDetails.numberOfSeasons ?? '--',
+                      ),
+                      InfoBox(
+                        typeInfo: 1,
+                        value: model.tvshowDetails.numberOfEpisodes ?? '--',
+                      ),
+                      InfoBox(
+                        typeInfo: 2,
+                        value: model.tvshowDetails.episodeRunTime.isNotEmpty
+                            ? model.tvshowDetails.episodeRunTime.first
+                            : 0,
+                      )
+                    ],
+                  ),
+                ),
+                Text(FlutterI18n.translate(context, 'app.modal.overview'),
+                    style: StyleText.MESSAGES),
+                const SizedBox(height: 8.0),
+                Expanded(
+                  flex: 6,
+                  child: SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Text(model.tvshowDetails.overview),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (inDatabase)
+            RandomButtonWidget(tvshowDetails: model.tvshowDetails)
+          else
+            SaveButton(id: idTv),
+        ],
+      ),
+    );
+  }
+}
