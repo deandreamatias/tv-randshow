7fb7d2f8ab101632872c786ecbe9243a1effe6f1
Matias de Andrea <deandreamatias@gmail.com>
2020-02-02 12:40:27 +0100

    Implement result view

---
+++ lib/ui/views/result_view.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+
+import '../../core/models/tvshow_details.dart';
+import '../../core/models/tvshow_result.dart';
+import '../shared/styles.dart';
+import '../shared/unicons_icons.dart';
+import '../widgets/info_box.dart';
+import 'loading_view.dart';
+
+class ResultView extends StatelessWidget {
+  const ResultView({Key key, this.tvshowDetails, this.tvshowResult})
+      : super(key: key);
+  final TvshowDetails tvshowDetails;
+  final TvshowResult tvshowResult;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: DEFAULT_INSESTS,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: DEFAULT_INSESTS,
+              child: Text(
+                FlutterI18n.translate(context, 'app.result.title'),
+                style: StyleText.MESSAGES,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                overflow: Overflow.visible,
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Positioned(
+                    right: 0.0,
+                    left: 0.0,
+                    top: 0.0,
+                    bottom: 36.0,
+                    child: Container(
+                      padding: DEFAULT_INSESTS,
+                      decoration: BoxDecoration(
+                          borderRadius: BORDER_RADIUS, border: Border.all()),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            tvshowResult.tvshowDetails.name,
+                            style: StyleText.TITLE,
+                          ),
+                          Flexible(
+                            fit: FlexFit.loose,
+                            flex: 2,
+                            child: Row(
+                              children: <Widget>[
+                                InfoBox(
+                                  typeInfo: 3,
+                                  value: tvshowResult.randomSeason,
+                                ),
+                                InfoBox(
+                                  typeInfo: 4,
+                                  value: tvshowResult.randomEpisode,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Text(tvshowResult.episodeName,
+                              style: StyleText.MESSAGES),
+                          const SizedBox(height: 8),
+                          Flexible(
+                            fit: FlexFit.loose,
+                            flex: 3,
+                            child: SingleChildScrollView(
+                              physics: const BouncingScrollPhysics(),
+                              child: Text(tvshowResult.episodeDescription),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: RaisedButton.icon(
+                      icon: const Icon(
+                        Unicons.dice_multiple,
+                        color: StyleColor.WHITE,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      color: StyleColor.PRIMARY,
+                      label: Text(
+                        FlutterI18n.translate(
+                            context, 'app.result.button_random'),
+                        style: StyleText.WHITE,
+                      ),
+                      onPressed: () => Navigator.push<LoadingView>(
+                        context,
+                        MaterialPageRoute<LoadingView>(
+                          builder: (BuildContext context) =>
+                              LoadingView(tvshowDetails: tvshowDetails),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: FlatButton.icon(
+                label: Text(
+                    FlutterI18n.translate(context, 'app.result.button_home')),
+                icon: const Icon(Unicons.home),
+                onPressed: () => Navigator.pushAndRemoveUntil<AppView>(
+                    context,
+                    MaterialPageRoute<AppView>(
+                      builder: (BuildContext context) => const AppView(),
+                    ),
+                    ModalRoute.withName('/')),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
