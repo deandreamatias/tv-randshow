a7bdea78e0e8ac6111eb842cd2b2c83995728ed4
Matias de Andrea <deandreamatias@gmail.com>
2020-02-08 13:30:36 +0100

    Fix load search list

--- lib/core/viewmodels/views/search_view_model.dart
+++ lib/core/viewmodels/views/search_view_model.dart
@@ -17,22 +17,7 @@ class SearchViewModel extends BaseModel {
   final ApiService _apiService;
   final SecureStorageService _secureStorageService;
 
-  bool searched = false;
-
-  void getSearch() {
-    setBusy(true);
-    searched = true;
-    setBusy(false);
-  }
-
-  void onSearch() {
-    setBusy(true);
-    searched = false;
-    setBusy(false);
-  }
-
   Future<List<Result>> loadList(String text, int page, String language) async {
-    setBusy(true);
     final Query query = Query(
       apiKey: await _secureStorageService.readStorage(KeyStore.API_KEY),
       language: language,
@@ -41,10 +26,8 @@ class SearchViewModel extends BaseModel {
     );
     final Search search = await _apiService.getSearch(query);
     if (search != null && search.results.isNotEmpty) {
-      setBusy(false);
       return search.results;
     } else {
-      setBusy(false);
       return null;
     }
   }
