90991577668241c688b313c2a7301714c412a37f
Matias de Andrea <deandreamatias@gmail.com>
2020-02-08 13:30:59 +0100

    Add debug log in base model

--- lib/core/viewmodels/base_model.dart
+++ lib/core/viewmodels/base_model.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import '../services/log_service.dart';
+
 class BaseModel extends ChangeNotifier {
+  final LogService _logger = LogService.instance;
   bool _busy = false;
   bool get busy => _busy;
 
   void setBusy(bool value) {
     _busy = value;
+    _logger.logger.i('State busy: $_busy');
     notifyListeners();
   }
 }
