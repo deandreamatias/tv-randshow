aeba8f9b714a7f7202c5b93faf9f660f41cf6cd8
Matias de Andrea <deandreamatias@gmail.com>
2020-06-16 17:51:48 +0200

    ci: Update prod build file

--- .tool/prod_build.cmd
+++ .tool/prod_build.cmd
@@ -3,5 +3,5 @@ cd android
 ./gradlew clean
 cd ..
 flutter clean
-flutter build appbundle --flavor prod -t lib/main_prod.dart
-flutter build apk --flavor prod -t lib/main_prod.dart
+flutter build appbundle --obfuscate --split-debug-info=/tv-randshow/split-debug --flavor prod -t lib/main_prod.dart
+flutter build apk --obfuscate --split-debug-info=/tv-randshow/split-debug --flavor prod -t lib/main_prod.dart
