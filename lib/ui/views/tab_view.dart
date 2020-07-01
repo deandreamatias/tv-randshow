97b0fc3e85c01f22ab9144f548a25ab5dd8bf302
Matias de Andrea <deandreamatias@gmail.com>
2020-07-01 18:05:15 +0200

    feat: Implement NavigationRail in big screens

--- lib/ui/views/tab_view.dart
+++ lib/ui/views/tab_view.dart
@@ -37,30 +37,71 @@ class _TabViewState extends State<TabView> {
       ),
       child: Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
-        body: _widgetOptions.elementAt(_selectedIndex),
-        bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            if (!kIsWeb)
-              BottomNavigationBarItem(
-                icon: const Icon(Unicons.favorite),
-                title: Text(translate('app.fav.tab')),
-              ),
-            BottomNavigationBarItem(
-              icon: const Icon(Unicons.search),
-              title: Text(translate('app.search.tab')),
-            ),
-            BottomNavigationBarItem(
-              icon: const Icon(Unicons.info_circle),
-              title: Text(translate('app.info.tab')),
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Theme.of(context).primaryColor,
-          onTap: (int index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) =>
+              constraints.maxWidth > 600
+                  ? Row(
+                      children: <Widget>[
+                        NavigationRail(
+                          labelType: NavigationRailLabelType.all,
+                          onDestinationSelected: (int value) {
+                            setState(() {
+                              _selectedIndex = value;
+                            });
+                          },
+                          destinations: <NavigationRailDestination>[
+                            if (!kIsWeb)
+                              NavigationRailDestination(
+                                icon: const Icon(Unicons.favorite),
+                                label: Text(translate('app.fav.tab')),
+                              ),
+                            NavigationRailDestination(
+                              icon: const Icon(Unicons.search),
+                              label: Text(translate('app.search.tab')),
+                            ),
+                            NavigationRailDestination(
+                              icon: const Icon(Unicons.info_circle),
+                              label: Text(translate('app.info.tab')),
+                            )
+                          ],
+                          selectedIndex: _selectedIndex,
+                        ),
+                        const VerticalDivider(thickness: 1, width: 1),
+                        Expanded(
+                          child: _widgetOptions.elementAt(_selectedIndex),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      children: <Widget>[
+                        Expanded(
+                          child: _widgetOptions.elementAt(_selectedIndex),
+                        ),
+                        BottomNavigationBar(
+                          currentIndex: _selectedIndex,
+                          onTap: (int value) {
+                            setState(() {
+                              _selectedIndex = value;
+                            });
+                          },
+                          items: <BottomNavigationBarItem>[
+                            if (!kIsWeb)
+                              BottomNavigationBarItem(
+                                icon: const Icon(Unicons.favorite),
+                                title: Text(translate('app.fav.tab')),
+                              ),
+                            BottomNavigationBarItem(
+                              icon: const Icon(Unicons.search),
+                              title: Text(translate('app.search.tab')),
+                            ),
+                            BottomNavigationBarItem(
+                              icon: const Icon(Unicons.info_circle),
+                              title: Text(translate('app.info.tab')),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
         ),
       ),
     );
