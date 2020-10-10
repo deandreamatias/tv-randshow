164c8ca93a81cadda002381563fbdd9ab0c3ea38
Matias de Andrea <deandreamatias@gmail.com>
2020-10-10 23:29:22 +0200

    feat: Get random episode from app link

---
+++ lib/core/models/tvshow_actions.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+class TvshowActions {
+  TvshowActions({this.tvshow});
+  factory TvshowActions.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return TvshowActions(
+      tvshow: map['tvshow'],
+    );
+  }
+  factory TvshowActions.fromJson(String source) =>
+      TvshowActions.fromMap(json.decode(source));
+
+  final String tvshow;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'tvshow': tvshow,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+}
--- lib/core/services/app_service.dart
+++ lib/core/services/app_service.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
 import 'package:package_info/package_info.dart';
+import 'package:uni_links/uni_links.dart';
+
+import '../models/tvshow_actions.dart';
 
 @lazySingleton
 class AppService {
@@ -8,5 +13,22 @@ class AppService {
   Future<String> getVersion() async {
     _packageInfo ??= await PackageInfo.fromPlatform();
     return _packageInfo.version;
+  }
+
+  Future<TvshowActions> initUniLinks() async {
+    try {
+      final Uri initialLink = await getInitialUri();
+      if (initialLink == null || initialLink.path.isEmpty) {
+        debugPrint('$runtimeType Link empty or null');
+        return TvshowActions(tvshow: '');
+      } else {
+        return initialLink.path.contains('getRandomEpisode')
+            ? TvshowActions.fromMap(initialLink.queryParameters)
+            : TvshowActions(tvshow: '');
+      }
+    } on PlatformException catch (e) {
+      return throw PlatformException(
+          code: e.code, message: e.message, details: e.details);
+    }
   }
 }
