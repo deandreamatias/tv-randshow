35c044d3912621aea7e6ee276cf1beefb2281eab
Matias de Andrea <deandreamatias@gmail.com>
2021-07-07 18:42:54 +0200

    test: Update integration tests

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,115 +1,93 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:tv_randshow/main_prod.dart' as app;
 
-void main() {
+Future<void> main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  group('TV Randshow app', () {
-    List<String> tvshows = ['The Office'];
+  final List<String> tvshows = ['The Office'];
 
-    testWidgets('initial app', (WidgetTester tester) async {
-      app.main();
-      await tester.pumpAndSettle();
+  testWidgets('initial app', (WidgetTester tester) async {
+    await app.main();
+    await tester.pumpAndSettle();
 
-      expect(find.byKey(Key('app.fav.title')), findsOneWidget);
-      expect(find.byKey(Key('app.fav.empty_message')), findsOneWidget);
-      expect(find.byKey(Key('app.fav.save')), findsOneWidget);
+    expect(find.byKey(Key('app.fav.title')), findsOneWidget);
+    expect(find.byKey(Key('app.fav.empty_message')), findsOneWidget);
+    expect(find.byKey(Key('app.fav.save')), findsOneWidget);
 
-      expect(find.text('Favorites'), findsOneWidget);
-      expect(find.text('Search'), findsOneWidget);
-      expect(find.text('Info'), findsOneWidget);
-    });
+    expect(find.text('Favorites'), findsOneWidget);
+    expect(find.text('Search'), findsOneWidget);
+    expect(find.text('Info'), findsOneWidget);
 
-    // test('search tv show', () async {
-    //   await tapKey('app.search.tab');
+    // Navigate to info tab
+    await tester.tap(find.byKey(Key('app.info.tab')));
+    await tester.pumpAndSettle();
 
-    //   await compareTextKey('app.search.message', 'Search a TV show in top bar');
-    //   expect(await driver.getText(find.text('Write a TV show name')),
-    //       equals('Write a TV show name'));
+    expect(find.byKey(Key('app.info.title')), findsOneWidget);
+    expect(find.byKey(Key('app.info.dark_title')), findsOneWidget);
+    expect(find.byKey(Key('app.info.web_title')), findsOneWidget);
+    expect(find.byKey(Key('app.info.rate_title')), findsOneWidget);
+    expect(find.byKey(Key('app.info.feedback_title')), findsOneWidget);
+    expect(find.byKey(Key('app.info.version.title')), findsOneWidget);
 
-    //   await tapKey('app.search.search_bar');
-    //   await driver.enterText(tvshows.first);
-    //   await pause(3500);
+    // Show version dialog
+    await tester.tap(find.byKey(Key('app.info.version.title')));
+    await tester.pumpAndSettle();
+    expect(find.byKey(Key('app.info.version.dialog_title')), findsOneWidget);
+    await tester.tap(find.byKey(Key('app.info.version.dialog_button')));
+  });
+  testWidgets('tv show flow', (WidgetTester tester) async {
+    final LocalizationDelegate delegate = await LocalizationDelegate.create(
+      fallbackLocale: 'en',
+      supportedLocales: <String>['en', 'es', 'pt'],
+    );
+    await tester.pumpWidget(LocalizedApp(delegate, app.MainApp()));
+    await tester.pumpAndSettle();
 
-    //   await compareTextKey('app.search.button_fav.2316', 'ADD TO FAV');
-    //   await tapKey('app.search.button_fav.2316');
-    //   await pause();
-    //   await compareTextKey('app.search.button_delete.2316', 'DELETE');
-    // });
-    // test('show favorites list', () async {
-    //   await tapKey('app.fav.tab');
+    // Navigate to search tab
+    await tester.tap(find.byKey(Key('app.search.tab')));
+    await tester.pumpAndSettle();
 
-    //   await driver.waitForAbsent(find.byValueKey('app.fav.loading'));
-    //   await pause(500);
-    //   await compareText(tvshows.first, tvshows.first);
-    // });
-    // test('get random episode', () async {
-    //   await compareTextKey('app.fav.button_random.2316', 'RANDOM');
-    //   await tapKey('app.fav.button_random.2316');
+    expect(find.byKey(Key('app.search.message')), findsOneWidget);
+    expect(find.text('Write a TV show name'), findsOneWidget);
 
-    //   await driver.waitForAbsent(find.byValueKey('app.loading.title'));
-    //   await compareTextKey(
-    //       'app.result.title', 'This is the TV show episode chosen at random!');
+    // Search tv show
+    expect(find.byKey(Key('app.search.search_bar')), findsOneWidget);
+    await tester.enterText(find.byType(TextField), tvshows.first);
+    await tester.pumpAndSettle(Duration(milliseconds: 3500));
 
-    //   await tapKey('app.result.button_random');
-    //   await driver.waitForAbsent(find.byValueKey('app.loading.title'));
-    //   await compareTextKey(
-    //       'app.result.title', 'This is the TV show episode chosen at random!');
+    // Save tv show
+    expect(find.byKey(Key('app.search.button_fav.2316')), findsOneWidget);
+    await tester.tap(find.byKey(Key('app.search.button_fav.2316')));
+    await tester.pumpAndSettle();
+    expect(find.byKey(Key('app.search.button_delete.2316')), findsOneWidget);
 
-    //   await tapKey('app.loading.button_fav');
-    //   await pause(500);
-    // });
+    // Navigate to favorites tab
+    await tester.tap(find.byKey(Key('app.fav.tab')));
+    await tester.pumpAndSettle();
+    expect(find.byKey(Key('app.fav.button_random.2316')), findsOneWidget);
 
-    // test('delete favorite show', () async {
-    //   await compareText(tvshows.first, tvshows.first);
+    // Get random episode
+    await tester.tap(find.byKey(Key('app.fav.button_random.2316')));
+    await tester.pumpAndSettle(Duration(milliseconds: 1000));
+    expect(find.byKey(Key('app.result.title')), findsOneWidget);
 
-    //   await tapKey('delete:1');
-    //   await driver.waitFor(find.byValueKey('app.delete_dialog.title'));
-    //   await tapKey('app.delete_dialog.button_delete');
-    //   await pause(500);
-    //   await driver.waitForAbsent(find.text(tvshows.first));
+    await tester.tap(find.byKey(Key('app.result.button_random')));
+    await tester.pumpAndSettle(Duration(milliseconds: 1000));
+    expect(find.byKey(Key('app.result.title')), findsOneWidget);
 
-    //   await pause(500);
-    // });
+    // Delete tv show
+    await tester.tap(find.byKey(Key('app.loading.button_fav')));
+    await tester.pumpAndSettle();
+    expect(find.byKey(Key('app.fav.button_random.2316')), findsOneWidget);
 
-    // test('info view', () async {
-    //   await tapKey('app.info.tab');
-
-    //   await compareTextKey('app.info.title', 'Info about TV Randshow');
-    //   await compareTextKey('app.info.web_title', 'Web app');
-    //   await compareTextKey('app.info.rate_title', 'Review');
-    //   await compareTextKey('app.info.feedback_title', 'Feedback');
-    //   await compareTextKey('app.info.version.title', 'Version changelog');
-
-    //   await tapKey('app.info.version.title');
-    //   await driver.waitFor(find.byValueKey('app.info.version.dialog_title'));
-    //   await tapKey('app.info.version.dialog_button');
-
-    //   await compareTextKey('app.info.dark_title', 'Dark theme');
-
-    //   await pause(500);
-    // });
+    await tester.tap(find.byKey(Key('delete:2316')));
+    await tester.pumpAndSettle();
+    expect(find.byKey(Key('app.delete_dialog.title')), findsOneWidget);
+    await tester.tap(find.byKey(Key('app.delete_dialog.button_delete')));
+    await tester.pumpAndSettle(Duration(milliseconds: 500));
+    expect(find.byKey(Key('app.fav.button_random.2316')), findsNothing);
   });
 }
-
-// /// Tap widget by [key] and pause 300 ms
-// Future<void> tapKey(String key) async {
-//   await driver.tap(find.byValueKey(key));
-//   await pause();
-// }
-
-// /// Get text by [key] and compare to [text]
-// Future<void> compareTextKey(String key, String text) async {
-//   expect(await driver.getText(find.byValueKey(key)), equals(text));
-// }
-
-// /// Get text by [value] and compare with [text]
-// Future<void> compareText(String value, String text) async {
-//   expect(await driver.getText(find.text(value)), equals(text));
-// }
-
-// Future<void> pause([int miliseconds = 300]) async {
-//   await Future<Duration>.delayed(Duration(milliseconds: miliseconds));
-// }
