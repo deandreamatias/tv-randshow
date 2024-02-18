7d2ae7d4b32d449f79ee302a55f155f3dbcc5437
Matias de Andrea <deandreamatias@gmail.com>
2024-02-18 14:37:28 +0100

    build: improve setup command

--- tool/setup.sh
+++ tool/setup.sh
@@ -4,8 +4,10 @@ clean="flutter clean"
 pub_get="flutter pub get"
 build_runner="dart run build_runner build --delete-conflicting-outputs"
 
+# Check if fvm is installed
 if command -v fvm &> /dev/null; then
-    fvm $clean &&
+    fvm doctor &&
+        fvm $clean &&
         fvm $pub_get &&
         fvm $build_runner
 else
