c8d588412cadba03691f50278b57139a1c964c80
Matias de Andrea <deandreamatias@gmail.com>
2025-04-17 23:43:41 +0200

    refactor: replace web dependency

--- lib/ui/shared/helpers/helpers_web.dart
+++ lib/ui/shared/helpers/helpers_web.dart
@@ -1,6 +1,6 @@
 // Can be ignore this warning because the files that use this helper, use conditional import.
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show window;
+import 'package:web/web.dart' as html;
 
 String getLocale() {
   return html.window.navigator.language;
