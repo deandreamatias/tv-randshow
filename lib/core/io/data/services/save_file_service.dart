ea674a99859283d4c371dd25d167027eb5787bc5
Matias de Andrea <deandreamatias@gmail.com>
2025-11-25 13:48:47 +0100

    fix: allow export favs on web

--- lib/core/io/data/services/save_file_service.dart
+++ lib/core/io/data/services/save_file_service.dart
@@ -1,6 +1,5 @@
-import 'dart:typed_data';
-
 import 'package:file_saver/file_saver.dart';
+import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:tv_randshow/core/io/domain/interfaces/i_manage_files_service.dart';
 
@@ -8,12 +7,19 @@ import 'package:tv_randshow/core/io/domain/interfaces/i_manage_files_service.dar
 class SaveFileService implements IManageFilesService {
   @override
   Future<String> saveFile(String fileName, String json) async {
-    return (await FileSaver.instance.saveAs(
-          name: fileName,
-          bytes: Uint8List.fromList(json.codeUnits),
-          fileExtension: 'json',
-          mimeType: MimeType.json,
-        )) ??
-        '';
+    final result = kIsWeb
+        ? await FileSaver.instance.saveFile(
+            name: fileName,
+            bytes: Uint8List.fromList(json.codeUnits),
+            fileExtension: 'json',
+            mimeType: MimeType.json,
+          )
+        : await FileSaver.instance.saveAs(
+            name: fileName,
+            bytes: Uint8List.fromList(json.codeUnits),
+            fileExtension: 'json',
+            mimeType: MimeType.json,
+          );
+    return result ?? '';
   }
 }
