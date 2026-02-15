31e78fa41c0a203aad60b1f19980baaa1300493f
Matias de Andrea <deandreamatias@gmail.com>
2026-02-15 14:57:54 +0100

    build: update build script

--- tool/setup.sh
+++ tool/setup.sh
@@ -3,6 +3,7 @@
 clean="flutter clean"
 pub_get="flutter pub get"
 build_runner="dart run build_runner build --delete-conflicting-outputs"
+android_clean="rm -f -r android/.gradle && rm -f -r android/build && rm -f -r android/app/.gradle && rm -f -r android/app/build"
 
-$clean && $pub_get && $build_runner
+$android_clean && $clean && $pub_get && $build_runner
 
