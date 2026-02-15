abf120586a2c1dc5c8feec48c2e31e8a1f734478
Matias de Andrea <deandreamatias@gmail.com>
2026-02-15 14:48:40 +0100

    build: update Android setup to allow 16 KB pages requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,15 +29,15 @@ val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "
 android {
     namespace = "deandrea.matias.tv_randshow"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14033849"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     defaultConfig {
